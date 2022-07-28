f9fca28aa060a31a251275411f17e37537f52b10
Antonio Alcantar <antonio.alcantar@people-media.com.mx>
2022-07-28 10:47:43 -0500

    Se prepara configuración para liberación a QA sobre Openshift

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,21 @@
 export const environment = {
   production: true,
   api: {
-    baseUrl: 'http://localhost:8080/api'
+    mssintetransOauth: 'mssintetrans-oauth/api',
+    mssintetransSIAP: 'mssintetrans-siap/siap/matricula',
+    mssintetransUnidad: 'mssintetrans-unidad/unidades',
+    mssintetransVehiculosEnajenacion: 'mssintetrans-vehiculos/estatus-enajenacion',
+    mssintetransUsuario:'mssintetrans-usuario/usuarios',
+    mssintetransRol:'mssintetrans-usuario/roles',
+    mssintetransOoad:'mssintetrans-usuario/ooad',
+    mssintetransUsuarioUnidad:'mssintetrans-usuario/unidades',
+    mssintetransCodigoPostal: 'mssintetrans-unidad/codigo-postal',
+    mssintetransCatalogoOoad: 'mssintetrans-unidad/ooad',
+    mssintetransTarjetaElectronica: 'mssintetrans-tarjetas-electronicas/tarjetas',
+    mssintetransVehiculosPropios: 'mssintetrans-vehiculos/propios',
+    mssintetransVehiculosArrendados : 'mssintetrans-vehiculos/arrendados',
+    mssintetransChoferes: 'mssintetrans-choferes/choferes',
+    mssintetransAseguradoras : 'mssintetrans-aseguradoras/aseguradora',
+    mssintetransCargaArchivos : 'mssintetrans-carga-archivos'
   }
 };
